649150fa0639b76372f75c209fcdce54dde967df
monil1301 <70565653+monil1301@users.noreply.github.com>
2022-01-23 01:20:46 +0530

    Added RS_TextFieldWithIcon.kt.kt along with icons required in authentication screen

--- app/src/main/java/com/shah/android/app/rentalstay/ui/theme/Dimensions.kt
+++ app/src/main/java/com/shah/android/app/rentalstay/ui/theme/Dimensions.kt
@@ -4,11 +4,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 // Text size
+val TextSize12 = 12.sp
 val TextSize14 = 14.sp
 val TextSize21 = 21.sp
 
 // Dimensions
 val Dimen0 = 0.dp
 val Dimen10 = 10.dp
+val Dimen12 = 12.dp
 val Dimen20 = 20.dp
+val Dimen24 = 24.dp
 val Dimen30 = 30.dp
